14e4820a1e0f387aebf1d7fed1e688e5ab5cd393
Vincent Verbergt <vincent.verbergt@student.uantwerpen.be>
2024-04-11 01:50:22 +0200

    updated notebooks

--- chatbot_domain/__init__.py
+++ chatbot_domain/__init__.py
@@ -7,7 +7,7 @@ hf_logging.set_verbosity_warning()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler(f"logs./{__name__}.log")
+fh = logging.FileHandler(f"logs/{__name__}.log")
 fh.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler()
